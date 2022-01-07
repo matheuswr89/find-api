40397be5a049f05fdba05c8d9a50d3f7436224ae
Matheus <matheuswr89@outlook.com>
2022-01-06 23:37:01 -0300

    fix bugs api

--- api/api.py
+++ api/api.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api, reqparse
 from find import check_arguments
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
